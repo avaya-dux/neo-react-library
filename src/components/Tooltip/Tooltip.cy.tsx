5292f451f338d8a0f7e92b2c97042bd93602c143
Joe S <jsebast@avaya.com>
2022-08-16 08:12:56 -0700

    add corner cases to tooltip cypress tests

--- src/components/Tooltip/Tooltip.cy.tsx
+++ src/components/Tooltip/Tooltip.cy.tsx
@@ -28,6 +28,8 @@ describe("Tooltip component", () => {
     const tooltipPositionClass = (position: Omit<TooltipPosition, "auto">) =>
       `neo-tooltip--${translatePositionToCSSName(position)}`;
 
+    // NOTE: the below are in orer of precedence, as defined by `helper.tsx/getIdealTooltipPosition`
+
     it("should assign the `top` position to a tooltip that has enough space above and to each side of itself", () => {
       cy.mount(
         <section className="heigh-ten-rem center-element">
@@ -43,6 +45,42 @@ describe("Tooltip component", () => {
       cy.get(tooltipContainer).should(
         "have.class",
         tooltipPositionClass("top")
+      );
+    });
+
+    it("should assign the `top-left` position to a tooltip that does not have enough space centered above itself, but does have enough space above and to it's left", () => {
+      cy.mount(
+        <section className="heigh-ten-rem center-right-align-element">
+          <Tooltip label={labelText}>
+            <Avatar />
+          </Tooltip>
+        </section>
+      );
+
+      cy.get(tooltipElement).should("not.be.visible");
+      cy.get(avatarElement).realHover();
+      cy.get(tooltipElement).should("be.visible");
+      cy.get(tooltipContainer).should(
+        "have.class",
+        tooltipPositionClass("top-left")
+      );
+    });
+
+    it("should assign the `top-right` position to a tooltip that does not have enough space centered above itself, nor to it's top-left, but does have enough space above to it's top-right", () => {
+      cy.mount(
+        <section className="heigh-ten-rem center-left-align-element">
+          <Tooltip label={labelText}>
+            <Avatar />
+          </Tooltip>
+        </section>
+      );
+
+      cy.get(tooltipElement).should("not.be.visible");
+      cy.get(avatarElement).realHover();
+      cy.get(tooltipElement).should("be.visible");
+      cy.get(tooltipContainer).should(
+        "have.class",
+        tooltipPositionClass("top-right")
       );
     });
 
@@ -64,10 +102,9 @@ describe("Tooltip component", () => {
       );
     });
 
-    it("should assign the `right` position to a tooltip that does not have enough space above, below, or left of itself, but does have space to the right of itself", () => {
-      cy.viewport(500, 90);
+    it("should assign the `bottom-left` position to a tooltip that does not have enough space centered below itself, but does have enough space below and to it's left", () => {
       cy.mount(
-        <section>
+        <section className="heigh-ten-rem right-align-element">
           <Tooltip label={labelText}>
             <Avatar />
           </Tooltip>
@@ -79,7 +116,25 @@ describe("Tooltip component", () => {
       cy.get(tooltipElement).should("be.visible");
       cy.get(tooltipContainer).should(
         "have.class",
-        tooltipPositionClass("right")
+        tooltipPositionClass("bottom-left")
+      );
+    });
+
+    it("should assign the `bottom-right` position to a tooltip that does not have enough space centered below itself, nor it's bottom-left, but does have enough space below and to it's bottom-right", () => {
+      cy.mount(
+        <section className="heigh-ten-rem left-align-element">
+          <Tooltip label={labelText}>
+            <Avatar />
+          </Tooltip>
+        </section>
+      );
+
+      cy.get(tooltipElement).should("not.be.visible");
+      cy.get(avatarElement).realHover();
+      cy.get(tooltipElement).should("be.visible");
+      cy.get(tooltipContainer).should(
+        "have.class",
+        tooltipPositionClass("bottom-right")
       );
     });
 
@@ -99,6 +154,25 @@ describe("Tooltip component", () => {
       cy.get(tooltipContainer).should(
         "have.class",
         tooltipPositionClass("left")
+      );
+    });
+
+    it("should assign the `right` position to a tooltip that does not have enough space above, below, or left of itself, but does have space to the right of itself", () => {
+      cy.viewport(500, 90);
+      cy.mount(
+        <section>
+          <Tooltip label={labelText}>
+            <Avatar />
+          </Tooltip>
+        </section>
+      );
+
+      cy.get(tooltipElement).should("not.be.visible");
+      cy.get(avatarElement).realHover();
+      cy.get(tooltipElement).should("be.visible");
+      cy.get(tooltipContainer).should(
+        "have.class",
+        tooltipPositionClass("right")
       );
     });
   });
