b532fcdd06328dbbbb13e789614301eecb3297cb
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2022-08-16 10:17:47 -0500

    add cypress (#2)
    * add cypress to project
    
    * config via: `yarn run cypress open`
    
    * fix cypress tsc issues, ignore generated files
    
    * add typings for vscode
    
    * add cypress generated files+folders to `clean` script
    
    * add linting for cypress, add support package, add minimal test
    
    * remove "default" storybook plugins (as they are always included)
    
    * update all storybook stories to use neo-css
    
    * simplify the cypress test
    
    * add 'auto' tests
    
    * cleanup
    
    improve wording, use helper methods
    
    * add final cypress tests for tooltip
    
    skipping the corner cases because I'm lazy
    
    * add corner cases to tooltip cypress tests
    
    * prettier
    
    Co-authored-by: Joe S <jsebast@avaya.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import "@avaya/neo/neo/dist/css/neo/neo.min.css";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
---
+++ cypress/support/component.ts
@@ -0,0 +1,22 @@
+import { mount } from "cypress/react";
+
+// ***********************************************************
+// This example support/component.ts is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+import "./commands";
+import "@avaya/neo/neo/dist/css/neo/neo.min.css";
+import "cypress-real-events/support";
+
+Cypress.Commands.add("mount", mount);
