763ecb4c11ceb4dc0c68830f6125e4ac4fc31fe3
Joe S <jsebast@avaya.com>
2022-08-16 09:15:08 -0700

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -40,4 +40,12 @@ export default defineConfig({
       },
     },
   },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+  },
 });
