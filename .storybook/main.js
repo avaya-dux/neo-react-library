f437bf9318353d21a2217bf72f41a40b5dc9e829
Joe S <jsebast@avaya.com>
2022-08-16 06:54:24 -0700

    remove "default" storybook plugins (as they are always included)

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,6 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-a11y",
-    "@storybook/addon-actions",
-    "@storybook/addon-docs",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-links",
