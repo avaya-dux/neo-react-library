f138c4a1395d390385059ce7e27581cdfa7f47fd
mrazian85 <61242168+mrazian85@users.noreply.github.com>
2022-08-23 11:24:49 -0400

    NEO-1070 - Remove sort icon, only render arrow up/down icon when column is sorted (#18)
    * Added logic to only render sorting icon when column is sorted, added tests to ensure behaviour works as intended
    
    * Added line to disable warning in build

--- src/react-table-config.d.ts
+++ src/react-table-config.d.ts
@@ -66,6 +66,7 @@ declare module "react-table" {
       // note that having Record here allows you to add anything to the options, this matches the spirit of the
       // underlying js library, but might be cleaner if it's replaced by a more specific type that matches your
       // feature set, this is a safe default.
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       Record<string, any> {}
 
   export interface Hooks<
