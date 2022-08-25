cd49d88ccf5f2628442238cc5382545b80a6bb98
Enrique Prado <enriquep@avaya.com>
2022-08-25 09:31:48 -0700

    Adding ref prop (using forwardRef)

--- src/components/Modal/InfoModal.tsx
+++ src/components/Modal/InfoModal.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { ReactNode, useId } from "react";
+import { forwardRef, ReactNode, useId } from "react";
 import ReactDOM from "react-dom";
 
 import "./BasicModal/BasicModal_shim.css";
@@ -27,52 +27,59 @@ export interface InfoModalProps {
 
  * @see https://design.avayacloud.com/components/web/modal-web
  */
-export const InfoModal = ({
-  children,
-  className,
-  id = useId(),
-  onClose,
-  open = false,
-  title,
-}: InfoModalProps) => {
-  if (!onClose) {
-    console.error("onClose prop is required.");
-  }
+export const InfoModal = forwardRef(
+  (
+    {
+      children,
+      className,
+      id = useId(),
+      onClose,
+      open = false,
+      title,
+    }: InfoModalProps,
+    ref: React.Ref<HTMLDivElement>
+  ) => {
+    if (!onClose) {
+      console.error("onClose prop is required.");
+    }
 
-  if (!open) {
-    return null;
-  }
+    if (!open) {
+      return null;
+    }
 
-  return ReactDOM.createPortal(
-    <div
-      id={id}
-      data-testid={id}
-      className={clsx("neo-modal-container neo-modal--active", className)}
-    >
-      <div className="neo-modal__background"></div>
+    return ReactDOM.createPortal(
       <div
-        className="neo-modal__content"
-        aria-label="This is an informational modal" // TODO: What should this value be? Ask Matt or Terri.
-        aria-modal="true"
-        role="dialog"
+        id={id}
+        data-testid={id}
+        className={clsx("neo-modal-container neo-modal--active", className)}
+        ref={ref}
       >
-        <div className="neo-modal__info-close">
-          <button
-            aria-label="close" // TODO: Localize this field.
-            className="neo-close"
-            onClick={onClose}
-          />
-        </div>
-        {title && (
-          <div className="neo-modal__header">
-            <h4>{title}</h4>
+        <div className="neo-modal__background"></div>
+        <div
+          className="neo-modal__content"
+          aria-label="This is an informational modal" // TODO: What should this value be? Ask Matt or Terri.
+          aria-modal="true"
+          role="dialog"
+        >
+          <div className="neo-modal__info-close">
+            <button
+              aria-label="close" // TODO: Localize this field.
+              className="neo-close"
+              onClick={onClose}
+            />
           </div>
-        )}
-        <div className="neo-modal__body">
-          <div className="neo-modal__message">{children}</div>
+          {title && (
+            <div className="neo-modal__header">
+              <h4>{title}</h4>
+            </div>
+          )}
+          <div className="neo-modal__body">
+            <div className="neo-modal__message">{children}</div>
+          </div>
         </div>
-      </div>
-    </div>,
-    document.body
-  );
-};
+      </div>,
+      document.body
+    );
+  }
+);
+InfoModal.displayName = "InfoModal";
