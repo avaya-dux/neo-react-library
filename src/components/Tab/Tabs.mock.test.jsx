e51b93824ddbcf9fe02ddfbcc41e173ae4a2b5b9
Joe S <jsebast@avaya.com>
2022-08-26 09:06:11 -0700

    fix `Tabs.mock.test.jsx` tests

--- src/components/Tab/Tabs.mock.test.jsx
+++ src/components/Tab/Tabs.mock.test.jsx
@@ -1,49 +1,47 @@
 import { composeStories } from "@storybook/testing-react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { vi } from "vitest";
 
 import {
   handleLeftCarouselMouseClickEvent,
   handleRightCarouselMouseClickEvent,
 } from "./EventHandlers";
 import { enableLeftButton, enableRightButton } from "./EventHandlers/Helper";
+
 import * as CarouselTabStories from "./Tabs.carousel.stories";
+const { ManyTabsCarousel } = composeStories(CarouselTabStories);
 
 vi.mock("./EventHandlers");
 vi.mock("./EventHandlers/Helper");
-const { ManyTabsCarousel } = composeStories(CarouselTabStories);
-import { vi } from "vitest";
 
 describe("Tabs", () => {
   describe("Storybook tests", () => {
     describe(ManyTabsCarousel.storyName, () => {
-      let renderResult;
       beforeEach(() => {
         enableLeftButton.mockReturnValue(true);
         enableRightButton.mockReturnValue(true);
-        renderResult = render(<ManyTabsCarousel />);
-      });
-      afterEach(() => {
+        render(<ManyTabsCarousel />);
         vi.resetAllMocks();
       });
-      it("When left carousel button is clicked, left button event handler is called", () => {
-        const { getAllByRole } = renderResult;
-        const buttons = getAllByRole("button");
+
+      it("When left carousel button is clicked, left button event handler is called", async () => {
+        const buttons = screen.getAllByRole("button");
         // carousel left, right, menu button
         expect(buttons.length).toBe(3);
 
         const leftButton = buttons[0];
-        userEvent.click(leftButton);
+        await userEvent.click(leftButton);
         expect(handleLeftCarouselMouseClickEvent).toBeCalled();
       });
-      it("When right carousel button is clicked, right button event handler is called", () => {
-        const { getAllByRole } = renderResult;
-        const buttons = getAllByRole("button");
+
+      it("When right carousel button is clicked, right button event handler is called", async () => {
+        const buttons = screen.getAllByRole("button");
         // carousel left, right, menu button
         expect(buttons.length).toBe(3);
 
         const rightButton = buttons[1];
-        userEvent.click(rightButton);
+        await userEvent.click(rightButton);
         expect(handleRightCarouselMouseClickEvent).toBeCalled();
       });
     });
