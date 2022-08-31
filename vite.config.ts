1fc8962a0ed59064b3b5a953c0ed5fe44380e416
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2022-08-31 16:30:56 -0500

    fix build issues (#26)
    * fix issue with `*.d.ts` files not being picked up
    
    * follow Vite docs more closely 
    
    Add `type: "module"`, and adjusted naming slightly of exported JS files. Tested in Enrique's Sample app.
    
    * only export map files on dev builds
    
    * bump version
    
    * undo tweak to more closely match Vite docs
    
    as it causes the storybook build to break
    
    Co-authored-by: Joe S <jsebast@avaya.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,50 +5,57 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
-export default defineConfig({
-  plugins: [react(), dts()],
-  resolve: {
-    alias: {
-      components: "/src/components",
-      utils: "/src/utils",
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      dts({
+        insertTypesEntry: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        components: "/src/components",
+        utils: "/src/utils",
+      },
     },
-  },
-  build: {
-    sourcemap: true,
-    lib: {
-      entry: "src/index.ts",
-      name: "@avaya/neo-react",
-      fileName: "avaya-neo-react",
-    },
-    // TODO: the css files doesn't seem to usable via the dp, need to figure out why
+    build: {
+      sourcemap: mode === "dev" ? true : false,
+      lib: {
+        entry: "src/index.ts",
+        name: "@avaya/neo-react",
+        fileName: "avaya-neo-react",
+      },
+      // TODO: the css files doesn't seem to usable via the dp, need to figure out why
 
-    // TODO: I'm (Joe) a big confused about how/why we need `rollupOptions`. Should probably do further research.
-    // most likely has to do with "library-mode"
-    // https://vitejs.dev/config/build-options.html#build-lib
-    // https://vitejs.dev/guide/build.html#library-mode
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "react",
-          "react-dom": "ReactDOM",
+      // TODO: I'm (Joe) a big confused about how/why we need `rollupOptions`. Should probably do further research.
+      // most likely has to do with "library-mode"
+      // https://vitejs.dev/config/build-options.html#build-lib
+      // https://vitejs.dev/guide/build.html#library-mode
+      rollupOptions: {
+        external: ["react", "react-dom"],
+        output: {
+          globals: {
+            react: "react",
+            "react-dom": "ReactDOM",
 
-          // other "globals" from old lib:
-          // "loglevel": "log",
-        },
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name == "style.css") return "avaya-neo-react.css";
-          return assetInfo.name;
+            // other "globals" from old lib:
+            // "loglevel": "log",
+          },
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name == "style.css") return "avaya-neo-react.css";
+            return assetInfo.name;
+          },
         },
       },
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-    coverage: {
-      reporter: ["text", "json", "json-summary", "html", "lcov"],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.ts",
+      coverage: {
+        reporter: ["text", "json", "json-summary", "html", "lcov"],
+      },
     },
-  },
+  };
 });
