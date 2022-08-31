ee8c1ce3545395c414f0b28a22602d706e78180d
Joe S <jsebast@avaya.com>
2022-08-31 11:43:37 -0700

    fix Toast tests

--- src/components/Toast/Toast.test.jsx
+++ src/components/Toast/Toast.test.jsx
@@ -1,6 +1,7 @@
 import { composeStories } from "@storybook/testing-react";
 import { render } from "@testing-library/react";
 import { axe } from "jest-axe";
+import { vi } from "vitest";
 
 import { popupHookLogger, popupManagerLogger } from "components/PopupManager";
 
@@ -16,26 +17,33 @@ const { Default, InteractiveToasts, IconBottomCenter, TwoToasts } =
 
 describe("Toast", () => {
   describe("Storybook", () => {
+    // NOTE: ignore react17 warnings (from storybook build). Should remove this line once storybook is updated to react18.
+    vi.spyOn(console, "error").mockImplementation(() => null);
+
     describe("Default", () => {
       let renderResult;
       beforeEach(() => {
         renderResult = render(<Default duration={100} />);
       });
+
       it("should render ok", () => {
         const { container } = renderResult;
         expect(container).toBeDefined();
       });
+
       it("passes basic axe compliance", async () => {
         const { container } = renderResult;
         const results = await axe(container);
         expect(results).toHaveNoViolations();
       });
     });
+
     describe("InteractiveToasts", () => {
       let renderResult;
       beforeEach(() => {
         renderResult = render(<InteractiveToasts />);
       });
+
       it("should render ok", () => {
         const { container } = renderResult;
         expect(container).toBeDefined();
@@ -47,11 +55,13 @@ describe("Toast", () => {
         expect(results).toHaveNoViolations();
       });
     });
+
     describe("IconBottomCenter", () => {
       let renderResult;
       beforeEach(() => {
         renderResult = render(<IconBottomCenter />);
       });
+
       it("should render ok", () => {
         const { container } = renderResult;
         expect(container).toBeDefined();
@@ -63,11 +73,13 @@ describe("Toast", () => {
         expect(results).toHaveNoViolations();
       });
     });
+
     describe("TwoToasts", () => {
       let renderResult;
       beforeEach(() => {
         renderResult = render(<TwoToasts />);
       });
+
       it("should render ok", () => {
         const { container } = renderResult;
         expect(container).toBeDefined();
