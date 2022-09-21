cf975eafe9d885ff4d4e6fd101e77cddb5bdfcc2
Enrique Prado <enriquep@avaya.com>
2022-09-20 18:18:01 -0700

    Demo checkpoint.
    Custom breakpoints and footer not yet done.

--- src/components/Layout/AppLayout.tsx
+++ src/components/Layout/AppLayout.tsx
@@ -1,24 +1,57 @@
-import { ReactNode } from "react";
-import { Composition } from "atomic-layout";
+import { ReactNode, useEffect } from "react";
+import Layout, { Composition } from "atomic-layout";
+
 
 export interface AppLayoutProps {
   desktopAreas?: string;
-  leftPanel: ReactNode;
-  mobileAreas?: string;
+  header?: ReactNode;
+  leftPanel?: ReactNode;
   mainContent: ReactNode;
-  header: ReactNode;
+  mobileAreas?: string;
+  rightPanel?: ReactNode;
 }
 
 export const AppLayout = ({
   desktopAreas,
+  header,
   leftPanel,
   mainContent,
   mobileAreas,
-  header,
+  rightPanel,
 }: AppLayoutProps) => {
+
+    useEffect(() => {
+      Layout.configure({
+        defaultBreakpointName: "desktop",
+        breakpoints: {
+          mobile: {
+            maxWidth: 767,
+          },
+          tablet: {
+            minWidth: 768,
+            maxWidth: 1023,
+          },
+          desktop: {
+            minWidth: 1024,
+            maxWidth: 1439,
+          },
+          bigscreen: {
+            minWidth: 1024,
+            maxWidth: 1439,
+          },
+        },
+      });
+    }, []);
+
   const defaultDesktopAreas = `
+    topheader topheader
+    leftpanel main
+    footer footer
+    `;
+
+    const defaultBigscreenAreas = `
     topheader topheader topheader
-    leftpanel main  rightpanel
+    leftpanel main rightpanel
     footer footer footer
     `;
 
@@ -32,6 +65,7 @@ export const AppLayout = ({
     <Composition
       areas={mobileAreas || defaultMobileAreas}
       areasMd={desktopAreas || defaultDesktopAreas}
+      areasLg={desktopAreas || defaultBigscreenAreas}
       templateRows={"60px 1fr 30px"}
       templateCols={"1fr auto"}
       templateColsMd={"250px 1fr auto"}
@@ -39,16 +73,11 @@ export const AppLayout = ({
     >
       {(Areas) => (
         <>
-          11:56
           <Areas.Topheader>{header}</Areas.Topheader>
           <Areas.Leftpanel area={"leftpanel"}>{leftPanel}</Areas.Leftpanel>
           <Areas.Main area={"main"}>{mainContent}</Areas.Main>
-          <Areas.Rightpanel area={"rightpanel"}>
-            Right Panel goes here
-          </Areas.Rightpanel>
-          <Areas.Footer area={"footer"}>
-            Footer goes here
-          </Areas.Footer>
+          <Areas.Rightpanel area={"rightpanel"}>{rightPanel}</Areas.Rightpanel>
+          <Areas.Footer area={"footer"}>Footer goes here</Areas.Footer>
         </>
       )}
     </Composition>
