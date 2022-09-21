a31abe1a21d4256c428e20052e46906e4fb2ac44
Enrique Prado <enriquep@avaya.com>
2022-09-21 14:19:49 -0700

    NEO-1171: AppLayout component Part 1 (#43)
    * Added atomic-layout and styled-components dep
    
    * AppLayout basic functionality done
    
    Mobile and desktop working inside Sample Neo App
    
    * Adding footer and right panel
    
    * Demo checkpoint.
    
    Custom breakpoints and footer not yet done.
    
    * Hiding AppLayout component for now
    
    * Formatting changes

---
+++ src/components/Layout/AppLayout.tsx
@@ -0,0 +1,83 @@
+import Layout, { Composition } from "atomic-layout";
+import { ReactNode, useEffect } from "react";
+
+export interface AppLayoutProps {
+  desktopAreas?: string;
+  header?: ReactNode;
+  leftPanel?: ReactNode;
+  mainContent: ReactNode;
+  mobileAreas?: string;
+  rightPanel?: ReactNode;
+}
+
+export const AppLayout = ({
+  desktopAreas,
+  header,
+  leftPanel,
+  mainContent,
+  mobileAreas,
+  rightPanel,
+}: AppLayoutProps) => {
+  useEffect(() => {
+    Layout.configure({
+      defaultBreakpointName: "desktop",
+      breakpoints: {
+        mobile: {
+          maxWidth: 767,
+        },
+        tablet: {
+          minWidth: 768,
+          maxWidth: 1023,
+        },
+        desktop: {
+          minWidth: 1024,
+          maxWidth: 1439,
+        },
+        bigscreen: {
+          minWidth: 1024,
+          maxWidth: 1439,
+        },
+      },
+    });
+  }, []);
+
+  const defaultDesktopAreas = `
+    topheader topheader
+    leftpanel main
+    footer footer
+    `;
+
+  const defaultBigscreenAreas = `
+    topheader topheader topheader
+    leftpanel main rightpanel
+    footer footer footer
+    `;
+
+  const defaultMobileAreas = `
+    topheader
+    main
+    footer
+    `;
+
+  return (
+    <Composition
+      areas={mobileAreas || defaultMobileAreas}
+      areasMd={desktopAreas || defaultDesktopAreas}
+      areasLg={desktopAreas || defaultBigscreenAreas}
+      templateRows={"60px 1fr 30px"}
+      templateCols={"1fr auto"}
+      templateColsMd={"250px 1fr auto"}
+      gap={1}
+    >
+      {(Areas) => (
+        <>
+          <Areas.Topheader>{header}</Areas.Topheader>
+          <Areas.Leftpanel area={"leftpanel"}>{leftPanel}</Areas.Leftpanel>
+          <Areas.Main area={"main"}>{mainContent}</Areas.Main>
+          <Areas.Rightpanel area={"rightpanel"}>{rightPanel}</Areas.Rightpanel>
+          <Areas.Footer area={"footer"}>Footer goes here</Areas.Footer>
+        </>
+      )}
+    </Composition>
+  );
+};
