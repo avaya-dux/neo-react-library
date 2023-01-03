98b396336c452f7319c44a7f03ff0577e71788e6
Joe S <jsebast@avaya.com>
2023-01-03 13:32:14 -0600

    update tests for updated functionality

--- src/components/LeftNav/LeftNav.test.tsx
+++ src/components/LeftNav/LeftNav.test.tsx
@@ -1,6 +1,8 @@
-import { render, RenderResult } from "@testing-library/react";
+import { render, RenderResult, screen } from "@testing-library/react";
 import { axe } from "jest-axe";
+import { vi } from "vitest";
 
+import { LeftNav } from "./";
 import {
   CategoryGroups,
   CategoryGroupsWithIcons,
@@ -8,96 +10,131 @@ import {
   DoesNotConflictWithOtherNavs,
 } from "./LeftNav.stories";
 
-describe("LeftNav storybook tests", () => {
-  describe("Default", () => {
-    let renderResult: RenderResult<
-      typeof import("@testing-library/dom/types/queries"),
-      HTMLElement,
-      HTMLElement
-    >;
+describe("LeftNav", () => {
+  describe("base tests", () => {
+    const examplechildren = (
+      <>
+        <LeftNav.TopLinkItem label="Active by default" href="#active" />
 
-    beforeEach(() => {
-      renderResult = render(<Default />);
+        <LeftNav.TopLinkItem label="Link 2" href="#test2" disabled />
+
+        <LeftNav.NavCategory expanded={true} label="Text Only Category">
+          <LeftNav.LinkItem href="http://first.com">
+            First Item
+          </LeftNav.LinkItem>
+          <LeftNav.LinkItem href="http://avaya.com" disabled>
+            Disabled Item
+          </LeftNav.LinkItem>
+        </LeftNav.NavCategory>
+      </>
+    );
+
+    it("render without exploding", () => {
+      render(<LeftNav aria-label="Main Navigation">{examplechildren}</LeftNav>);
+
+      expect(screen.getByRole("navigation")).toBeInTheDocument();
     });
 
-    it("should render ok", () => {
-      const { container } = renderResult;
-      expect(container).not.toBe(null);
-    });
+    it("throws an error if no 'aria-label*` is passed", () => {
+      vi.spyOn(console, "error").mockImplementation(() => null);
 
-    it("passes basic axe compliance", async () => {
-      const { container } = renderResult;
-      const results = await axe(container);
-      expect(results).toHaveNoViolations();
-    });
-  });
-
-  describe("CategoryGroups", () => {
-    let renderResult: RenderResult<
-      typeof import("@testing-library/dom/types/queries"),
-      HTMLElement,
-      HTMLElement
-    >;
-
-    beforeEach(() => {
-      renderResult = render(<CategoryGroups />);
-    });
-
-    it("should render ok", () => {
-      const { container } = renderResult;
-      expect(container).not.toBe(null);
-    });
-
-    it("passes basic axe compliance", async () => {
-      const { container } = renderResult;
-      const results = await axe(container);
-      expect(results).toHaveNoViolations();
+      expect(() =>
+        render(<LeftNav aria-label="">{examplechildren}</LeftNav>)
+      ).toThrowError();
     });
   });
 
-  describe("CategoryGroupsWithIcons", () => {
-    let renderResult: RenderResult<
-      typeof import("@testing-library/dom/types/queries"),
-      HTMLElement,
-      HTMLElement
-    >;
+  describe("storybook tests", () => {
+    describe("Default", () => {
+      let renderResult: RenderResult<
+        typeof import("@testing-library/dom/types/queries"),
+        HTMLElement,
+        HTMLElement
+      >;
 
-    beforeEach(() => {
-      renderResult = render(<CategoryGroupsWithIcons />);
+      beforeEach(() => {
+        renderResult = render(<Default />);
+      });
+
+      it("should render ok", () => {
+        const { container } = renderResult;
+        expect(container).not.toBe(null);
+      });
+
+      it("passes basic axe compliance", async () => {
+        const { container } = renderResult;
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+      });
     });
 
-    it("should render ok", () => {
-      const { container } = renderResult;
-      expect(container).not.toBe(null);
+    describe("CategoryGroups", () => {
+      let renderResult: RenderResult<
+        typeof import("@testing-library/dom/types/queries"),
+        HTMLElement,
+        HTMLElement
+      >;
+
+      beforeEach(() => {
+        renderResult = render(<CategoryGroups />);
+      });
+
+      it("should render ok", () => {
+        const { container } = renderResult;
+        expect(container).not.toBe(null);
+      });
+
+      it("passes basic axe compliance", async () => {
+        const { container } = renderResult;
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+      });
     });
 
-    it("passes basic axe compliance", async () => {
-      const { container } = renderResult;
-      const results = await axe(container);
-      expect(results).toHaveNoViolations();
+    describe("CategoryGroupsWithIcons", () => {
+      let renderResult: RenderResult<
+        typeof import("@testing-library/dom/types/queries"),
+        HTMLElement,
+        HTMLElement
+      >;
+
+      beforeEach(() => {
+        renderResult = render(<CategoryGroupsWithIcons />);
+      });
+
+      it("should render ok", () => {
+        const { container } = renderResult;
+        expect(container).not.toBe(null);
+      });
+
+      it("passes basic axe compliance", async () => {
+        const { container } = renderResult;
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+      });
     });
-  });
 
-  describe("DoesNotConflictWithOtherNavs", () => {
-    let renderResult: RenderResult<
-      typeof import("@testing-library/dom/types/queries"),
-      HTMLElement,
-      HTMLElement
-    >;
+    describe("DoesNotConflictWithOtherNavs", () => {
+      let renderResult: RenderResult<
+        typeof import("@testing-library/dom/types/queries"),
+        HTMLElement,
+        HTMLElement
+      >;
 
-    beforeEach(() => {
-      renderResult = render(<DoesNotConflictWithOtherNavs />);
-    });
+      beforeEach(() => {
+        renderResult = render(<DoesNotConflictWithOtherNavs />);
+      });
 
-    it("should render ok", () => {
-      const { container } = renderResult;
-      expect(container).not.toBe(null);
-    });
+      it("should render ok", () => {
+        const { container } = renderResult;
+        expect(container).not.toBe(null);
+      });
 
-    it("passes basic axe compliance", async () => {
-      const { container } = renderResult;
-      const results = await axe(container);
-      expect(results).toHaveNoViolations();
+      it("passes basic axe compliance", async () => {
+        const { container } = renderResult;
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+      });
     });
   });
 });
