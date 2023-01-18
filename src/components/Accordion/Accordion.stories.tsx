19bd39c88e3d1981735d7b7827d8cb151b292fd1
Chunguang Yang <chungguangy@avaya.com>
2023-01-18 12:01:45 -0500

    remove space

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -11,15 +11,17 @@ export const Single = () => (
   <Sheet title="Accordion Examples" style={{ width: 400 }}>
     <Accordion header="Accordion Header">Some text in body</Accordion>
 
-    <Accordion header="Accordion with expand true " defaultExpanded>
+    <Accordion header="Accordion with expand true" defaultExpanded>
       Some text in body
     </Accordion>
 
     <Accordion header="Header Disabled" disabled />
 
     <Accordion header="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat harum in, ut possimus, porro accusamus beatae ab officiis quae consectetur quia? Fugiat ea, sunt fuga numquam accusamus nemo odio praesentium.">
-      Some text in body
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptatum corrupti voluptatibus perferendis magni, possimus animi provident dolor, quia repudiandae in id ullam quas expedita esse, laborum labore libero dolorem?
+      Some text in body Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Aliquid voluptatum corrupti voluptatibus perferendis magni, possimus animi
+      provident dolor, quia repudiandae in id ullam quas expedita esse, laborum
+      labore libero dolorem?
     </Accordion>
   </Sheet>
 );
