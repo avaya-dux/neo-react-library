89913bb4e37e050790ff4a84c96c962f6dcb6f6b
Joe S <jsebast@avaya.com>
2023-03-03 15:21:32 -0600

    fix more tests in Select

--- src/components/Select/Searchable.test.jsx
+++ src/components/Select/Searchable.test.jsx
@@ -140,17 +140,17 @@ describe("Select", () => {
       );
 
       const comboboxBtn = screen.getAllByRole("textbox")[0].closest("span");
-      await user.click(comboboxBtn);
+      await act(async () => await user.click(comboboxBtn));
 
       // pre search+add we have all options in the list
       expect(screen.getAllByRole("option")).toHaveLength(fruitOptions.length);
 
-      await user.keyboard(newOptionText);
+      await act(async () => await user.keyboard(newOptionText));
       expect(screen.getAllByRole("option")).toHaveLength(1);
       expect(screen.getByRole("option")).toHaveTextContent(newOptionText);
 
-      await user.keyboard(UserEventKeys.DOWN);
-      await user.keyboard(UserEventKeys.ENTER);
+      await act(async () => await user.keyboard(UserEventKeys.DOWN));
+      await act(async () => await user.keyboard(UserEventKeys.ENTER));
 
       // now that we've added the new option, we can now see the full list, excluding
       // the new option as we do not show that in the list
@@ -160,7 +160,7 @@ describe("Select", () => {
       expect(screen.getAllByRole("button")).toHaveLength(1);
       expect(screen.getByText(newOptionText)).toBeInTheDocument();
 
-      await user.keyboard(UserEventKeys.BACKSPACE);
+      await act(async () => await user.keyboard(UserEventKeys.BACKSPACE));
       expect(screen.queryAllByRole("button")).toHaveLength(0); // chip removed
     });
 
