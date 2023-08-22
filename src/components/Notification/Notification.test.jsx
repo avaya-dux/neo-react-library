621753250d92e1283661f87a9fe2585f52e69483
mrazian85 <mrazian@avaya.com>
2023-08-22 10:38:09 -0400

    Ran yarn all

--- src/components/Notification/Notification.test.jsx
+++ src/components/Notification/Notification.test.jsx
@@ -1,10 +1,5 @@
 import { composeStories } from "@storybook/testing-react";
-import {
-  fireEvent,
-  getByRole,
-  render,
-  screen,
-} from "@testing-library/react";
+import { fireEvent, getByRole, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { axe } from "jest-axe";
 import { vi } from "vitest";
