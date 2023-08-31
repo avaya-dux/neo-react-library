f5f13b92b18de1e80f4aca8ad60430031ec62111
Chunguang Yang <chungguangy@avaya.com>
2023-08-31 15:06:13 -0400

    disable logging

--- src/components/Select/utils/useDownshift.ts
+++ src/components/Select/utils/useDownshift.ts
@@ -5,7 +5,7 @@ import { Dispatch, SetStateAction, useState } from "react";
 import { SelectOptionProps } from "./SelectTypes";
 
 const logger = log.getLogger("use-downshfit");
-logger.enableAll();
+logger.disableAll();
 
 const createOptionValue = "neo-select-create-option";
 
