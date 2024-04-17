eb67f5c018afde06332888f2d4680417e9424fe8
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-04-17 10:32:02 -0500

    simplify static story

--- src/components/Notes/Notes.stories.tsx
+++ src/components/Notes/Notes.stories.tsx
@@ -15,6 +15,74 @@ export default meta;
 
 type Story = StoryObj<typeof Notes>;
 
+const interactions = [
+  <Notes.Interaction key={1}>
+    <Notes.Interaction.Heading>
+      <Icon icon="email-inbound" aria-label="email inbound" size="md" />
+
+      <p>March 19, 2024 | 12:50 PM</p>
+    </Notes.Interaction.Heading>
+
+    <Notes.Interaction.Message>
+      <Notes.Interaction.Message.Title>
+        March 19, 2024 | 12:50 AM
+      </Notes.Interaction.Message.Title>
+
+      <Notes.Interaction.Message.Content author="Me" self>
+        Client has sent an email. They did not receive any feedback on this
+        claim. They are quite upset. I have set up a meeting to call on Monday.
+      </Notes.Interaction.Message.Content>
+    </Notes.Interaction.Message>
+
+    <Notes.Interaction.Message>
+      <Notes.Interaction.Message.Title>
+        March 19, 2024 | 12:32 AM
+      </Notes.Interaction.Message.Title>
+
+      <Notes.Interaction.Message.Content author="Barbara Leyton">
+        Transferring this to you. I think we need to escalate this.
+      </Notes.Interaction.Message.Content>
+    </Notes.Interaction.Message>
+  </Notes.Interaction>,
+
+  <Notes.Interaction key={2}>
+    <Notes.Interaction.Heading>
+      <Icon icon="call-outbound" aria-label="call outbound" size="md" />
+
+      <p>March 15, 2024 | 12:28 AM</p>
+    </Notes.Interaction.Heading>
+
+    <Notes.Interaction.Message>
+      <Notes.Interaction.Message.Title>
+        May 10, 2024 | 12:28 AM
+      </Notes.Interaction.Message.Title>
+
+      <Notes.Interaction.Message.Content author="Barbara Leyton">
+        Client called to ask for an update. Sent an email to the claim
+        department. Need to call when we have an update.
+      </Notes.Interaction.Message.Content>
+    </Notes.Interaction.Message>
+  </Notes.Interaction>,
+
+  <Notes.Interaction key={3}>
+    <Notes.Interaction.Heading>
+      <Icon icon="call-outbound" aria-label="call outbound" size="md" />
+
+      <p>March 8, 2024 | 12:28 AM</p>
+    </Notes.Interaction.Heading>
+
+    <Notes.Interaction.Message>
+      <Notes.Interaction.Message.Title>
+        March 8, 2024 | 12:32 AM
+      </Notes.Interaction.Message.Title>
+
+      <Notes.Interaction.Message.Content author="Barbara Leyton">
+        Client has been in accident, and would like a claim started.
+      </Notes.Interaction.Message.Content>
+    </Notes.Interaction.Message>
+  </Notes.Interaction>,
+];
+
 export const StaticNotesExample: Story = {
   render: () => (
     <section className="notes-container">
@@ -27,72 +95,7 @@ export const StaticNotesExample: Story = {
           </Button>
         </Notes.Heading>
 
-        <Notes.Interaction>
-          <Notes.Interaction.Heading>
-            <Icon icon="email-inbound" aria-label="email inbound" size="md" />
-
-            <p>March 19, 2024 | 12:50 PM</p>
-          </Notes.Interaction.Heading>
-
-          <Notes.Interaction.Message>
-            <Notes.Interaction.Message.Title>
-              March 19, 2024 | 12:50 AM
-            </Notes.Interaction.Message.Title>
-
-            <Notes.Interaction.Message.Content author="Me" self>
-              Client has sent an email. They did not receive any feedback on
-              this claim. They are quite upset. I have set up a meeting to call
-              on Monday.
-            </Notes.Interaction.Message.Content>
-          </Notes.Interaction.Message>
-
-          <Notes.Interaction.Message>
-            <Notes.Interaction.Message.Title>
-              March 19, 2024 | 12:32 AM
-            </Notes.Interaction.Message.Title>
-
-            <Notes.Interaction.Message.Content author="Barbara Leyton">
-              Transferring this to you. I think we need to escalate this.
-            </Notes.Interaction.Message.Content>
-          </Notes.Interaction.Message>
-        </Notes.Interaction>
-
-        <Notes.Interaction>
-          <Notes.Interaction.Heading>
-            <Icon icon="call-outbound" aria-label="call outbound" size="md" />
-
-            <p>March 15, 2024 | 12:28 AM</p>
-          </Notes.Interaction.Heading>
-
-          <Notes.Interaction.Message>
-            <Notes.Interaction.Message.Title>
-              May 10, 2024 | 12:28 AM
-            </Notes.Interaction.Message.Title>
-
-            <Notes.Interaction.Message.Content author="Barbara Leyton">
-              Client called to ask for an update. Sent an email to the claim
-              department. Need to call when we have an update.
-            </Notes.Interaction.Message.Content>
-          </Notes.Interaction.Message>
-        </Notes.Interaction>
-
-        <Notes.Interaction>
-          <Notes.Interaction.Heading>
-            <Icon icon="call-outbound" aria-label="call outbound" size="md" />
-
-            <p>March 8, 2024 | 12:28 AM</p>
-          </Notes.Interaction.Heading>
-
-          <Notes.Interaction.Message>
-            <Notes.Interaction.Message.Title>
-              March 8, 2024 | 12:32 AM
-            </Notes.Interaction.Message.Title>
-
-            <Notes.Interaction.Message.Content author="Barbara Leyton">
-              Client has been in accident, and would like a claim started.
-            </Notes.Interaction.Message.Content>
-          </Notes.Interaction.Message>
-        </Notes.Interaction>
+        {interactions}
       </Notes>
     </section>
   ),
