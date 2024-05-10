58374d78507edf0e34b3a09246da26d4b64cfbc5
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-05-10 14:58:17 -0500

    more tests!

--- src/components/PanelTabs/PanelTabs.test.tsx
+++ src/components/PanelTabs/PanelTabs.test.tsx
@@ -72,6 +72,8 @@ describe("Panel Tabs", () => {
   describe("functionality tests", () => {
     const user = userEvent.setup();
 
+    const collapsedClassName = "neo-paneltabs__tabs-expand--invert";
+
     it("respects the `onClick` events", async () => {
       const mock = vi.fn();
 
@@ -125,6 +127,93 @@ describe("Panel Tabs", () => {
       const tabThree = screen.getByLabelText(tab3AriaLabel);
       await user.click(tabThree);
       expect(mock).toHaveBeenCalledWith(2);
+    });
+
+    it("toggles between expanded and collapsed", async () => {
+      const { container } = render(
+        <PanelTabs>
+          <PanelTabs.Panel>
+            <PanelTabs.PanelContent active>
+              <p>{panel1Content}</p>
+            </PanelTabs.PanelContent>
+          </PanelTabs.Panel>
+
+          <PanelTabs.TabsContainer>
+            <PanelTabs.TabItem
+              aria-label={tab1AriaLabel}
+              icon="email-inbound"
+            />
+          </PanelTabs.TabsContainer>
+        </PanelTabs>,
+      );
+
+      const toggleButton = container.querySelector(
+        ".neo-paneltabs__tabs-expand",
+      );
+      expect(toggleButton).toBeInTheDocument();
+      expect(toggleButton).not.toHaveClass(collapsedClassName);
+
+      await user.click(toggleButton as Element);
+      expect(toggleButton).toHaveClass(collapsedClassName);
+
+      await user.click(toggleButton as Element);
+      expect(toggleButton).not.toHaveClass(collapsedClassName);
+    });
+
+    it("expands on tab item click when collapsed", async () => {
+      render(
+        <PanelTabs defaultExpanded={false}>
+          <PanelTabs.Panel>
+            <PanelTabs.PanelContent active>
+              <p>{panel1Content}</p>
+            </PanelTabs.PanelContent>
+          </PanelTabs.Panel>
+
+          <PanelTabs.TabsContainer>
+            <PanelTabs.TabItem
+              active
+              aria-label={tab1AriaLabel}
+              icon="email-inbound"
+            />
+          </PanelTabs.TabsContainer>
+        </PanelTabs>,
+      );
+
+      const panelOne = screen.getByText(panel1Content);
+      expect(panelOne).not.toBeVisible();
+
+      const tabOne = screen.getByLabelText(tab1AriaLabel);
+      await user.click(tabOne);
+
+      expect(panelOne).toBeVisible();
+    });
+
+    it("collapses on active tab item click when expanded", async () => {
+      render(
+        <PanelTabs>
+          <PanelTabs.Panel>
+            <PanelTabs.PanelContent active>
+              <p>{panel1Content}</p>
+            </PanelTabs.PanelContent>
+          </PanelTabs.Panel>
+
+          <PanelTabs.TabsContainer>
+            <PanelTabs.TabItem
+              active
+              aria-label={tab1AriaLabel}
+              icon="email-inbound"
+            />
+          </PanelTabs.TabsContainer>
+        </PanelTabs>,
+      );
+
+      const panelOne = screen.getByText(panel1Content);
+      expect(panelOne).toBeVisible();
+
+      const tabOne = screen.getByLabelText(tab1AriaLabel);
+      await user.click(tabOne);
+
+      expect(panelOne).not.toBeVisible();
     });
   });
 });
