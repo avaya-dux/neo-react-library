facecf23dfaf0e3cf254987466d6c77b9b0d6c7e
Enrique Prado <enriquep@avaya.com>
2024-06-13 13:14:49 -0700

    Adding Form in Drawer story

--- src/components/Drawer/Drawer.stories.tsx
+++ src/components/Drawer/Drawer.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, Story } from "@storybook/react";
 import { useState } from "react";
 
-import { Button } from "components/Button";
+import { Button, Form, Switch, Toast } from "components";
 
 import { Drawer, type DrawerProps } from "./";
 
@@ -59,6 +59,72 @@ export const Default = () => {
 				closeOnBackgroundClick={false}
 			>
 				<p>This Drawer will not close if you click on the scrim background</p>
+			</Drawer>
+		</main>
+	);
+};
+
+export const withFormComponents = () => {
+	const [defaultDrawerOpen, setDefaultDrawerOpen] = useState(false);
+	const [noDismissDrawerOpen, setNoDismissDrawerOpen] = useState(false);
+
+	const toggleDrawerByName = (drawerName: string) => {
+		console.log("drawerName: ", drawerName);
+		switch (drawerName) {
+			case "default":
+				setDefaultDrawerOpen(!defaultDrawerOpen);
+				break;
+			case "no-dismiss":
+				setNoDismissDrawerOpen(!noDismissDrawerOpen);
+				break;
+		}
+	};
+
+	return (
+		<main>
+			<section
+				style={{
+					display: "flex",
+					justifyContent: "space-between",
+					marginBottom: "1rem",
+				}}
+			>
+				<Button onClick={() => toggleDrawerByName("default")}>
+					Toggle Drawer Open
+				</Button>
+			</section>
+
+			<Drawer
+				open={defaultDrawerOpen}
+				onClose={() => toggleDrawerByName("default")}
+				title="Drawer with Form and components"
+				closeOnBackgroundClick={false}
+			>
+				<Form
+					// biome-ignore lint/suspicious/noExplicitAny: we require maximum flexibility here
+					onSubmit={(e: any) => {
+						e.preventDefault();
+						alert("you successfully submitted");
+					}}
+					style={{ width: 300 }}
+				>
+					<p style={{ paddingBottom: 20 }}>
+						Terms of Service Example. User must accept ToS before being allowed
+						to proceed.
+					</p>
+
+					<Switch required name="ToS" value="accepted">
+						Do you accept the Terms of Service?
+					</Switch>
+
+					<section style={{ display: "flex", justifyContent: "space-between" }}>
+						<Button type="reset" variant="secondary">
+							Reset
+						</Button>
+
+						<Button type="submit">Submit</Button>
+					</section>
+				</Form>
 			</Drawer>
 		</main>
 	);
