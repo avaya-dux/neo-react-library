2006b2e26a0d7ac03f484f920f620a5d1d63a7fe
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-07-11 16:34:30 -0500

    biome is being odd, removing problematic line

--- src/components/Table/TableComponents/TableBody.tsx
+++ src/components/Table/TableComponents/TableBody.tsx
@@ -216,7 +216,6 @@ const TableDataRows: TableBodyComponentType = ({
 				)}
 
 				{row.cells.map((cell, i) => (
-					// biome-ignore lint/suspicious/noArrayIndexKey: acceptable risk
 					<td {...cell.getCellProps()} key={`td-${i}`}>
 						{cell.render("Cell")}
 					</td>
