7483d45c1b6eddba4d9c5993bbe06f44a9ad84e9
Chunguang Yang <chungguangy@avaya.com>
2024-07-23 19:36:07 -0400

    add javascript doc

--- src/components/SplitButton/SplitButton.tsx
+++ src/components/SplitButton/SplitButton.tsx
@@ -1,12 +1,20 @@
 import clsx from "clsx";
 import { useMemo, type DetailedHTMLProps, type HTMLAttributes } from "react";
-
-import "./SplitButton_shim.css";
 import { Button } from "components/Button";
 import { Menu, MenuButton, MenuItem, type MenuProps } from "components/Menu";
-import type { IconNamesType } from "utils";
 import type { MenuChildrenType } from "components/Menu/MenuTypes";
+import type { IconNamesType } from "utils";
 
+import "./SplitButton_shim.css";
+
+/**
+ * SplitButtonProps
+ * @prop {MenuChildrenType} children - The menu items to display in the dropdown.
+ * @prop {string} [className] - The class name to add to the component.
+ * @prop {"sm" | "md"} [height] - The height of the SplitButton.
+ * @prop {MenuProps} [menuProps] - The props to pass to the Menu component.
+ * @prop {ButtonProps} buttonProps - The props to pass to the Button component.
+ */
 export interface SplitButtonProps
 	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
 	buttonProps: {
@@ -16,11 +24,32 @@ export interface SplitButtonProps
 		onClick?: () => void;
 	};
 
-	menuProps?: Omit<MenuProps, "menuRootElement" | "children">;
+	menuProps?: Omit<MenuProps, "menuRootElement" | "children" | "itemAlignment">;
 	children: MenuChildrenType;
 	height?: "sm" | "md";
 }
 
+/**
+ * SplitButton is a button that has a dropdown menu attached to it.
+ * It is used to provide a primary action and secondary actions in a single component.
+ * The primary action is a button that can have an icon and text.
+ * The secondary actions are menu items that are displayed in a dropdown menu.
+ * The SplitButton is used to save space in the UI and provide a clean user experience.
+ * The SplitButton is used in the Neo design system.
+ * @param {SplitButtonProps} props
+ * @example
+ * <SplitButton
+ *    buttonProps={{
+ *      variant: "primary",
+ *      text: "Primary Button",
+ *      onClick: () => console.log("Primary Button Clicked"),
+ *      icon: "settings",
+ *    }}
+ * >
+ *   <MenuItem onClick={() => console.log("Menu Item 1 Clicked")}>Menu Item 1</MenuItem>
+ *   <MenuItem onClick={() => console.log("Menu Item 2 Clicked")}>Menu Item 2</MenuItem>
+ * </SplitButton>
+ */
 export const SplitButton = ({
 	buttonProps: { variant = "primary", text: buttonText, onClick, icon } = {},
 	height = "md",
@@ -48,7 +77,7 @@ export const SplitButton = ({
 			className={clsx(
 				"neo-splitbutton",
 				className,
-				height === "md" && "neo-splitbutton--midium-height",
+				height === "md" && "neo-splitbutton--medium-height",
 				height === "sm" && "neo-splitbutton--small-height",
 				variant === "secondary" && "neo-splitbutton--secondary",
 			)}
