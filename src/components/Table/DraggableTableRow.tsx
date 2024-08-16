2a1c267c50e9e2e414214d6303433bd0a6f98d4a
Chunguang Yang <chungguangy@avaya.com>
2024-08-16 11:11:47 -0400

    wrap tr with tbody

--- src/components/Table/DraggableTableRow.tsx
+++ src/components/Table/DraggableTableRow.tsx
@@ -42,42 +42,46 @@ export const DraggableTableRow = <T extends Record<string, any>>({
 	const cellCount = row.cells.length + 1 + (checkboxTd ? 1 : 0);
 
 	return (
-		<tr
-			ref={setNodeRef}
-			role={preparedRowProps.role}
-			style={{ ...style, ...preparedRowProps.style }}
-			key={preparedRowProps.key || `table-row-${row.id}`}
-			className={clsx(
-				row.isSelected && "active",
-				preparedRowProps.className,
-				row.original.disabled && "disabled",
-			)}
-			tabIndex={0}
-		>
-			{isDragging ? (
-				<EmptyRow colSpan={cellCount}>&nbsp;</EmptyRow>
-			) : (
-				<>
-					<td className="neo-table__dnd-td">
-						<DragHandle
-							ref={setActivatorNodeRef}
-							{...attributes}
-							{...listeners}
-						/>
-					</td>
-					{checkboxTd && (
-						<td style={{ padding: "0px 0px 0px 5px" }}>{checkboxTd}</td>
-					)}
-					{row.cells.map((cell) => {
-						const { key, ...restCellProps } = cell.getCellProps();
-						return (
-							<td {...restCellProps} key={key}>
-								{cell.render("Cell")}
-							</td>
-						);
-					})}
-				</>
-			)}
-		</tr>
+		<tbody ref={setNodeRef}>
+			<tr
+				role={preparedRowProps.role}
+				style={{ ...style, ...preparedRowProps.style }}
+				key={preparedRowProps.key || `table-row-${row.id}`}
+				className={clsx(
+					row.isSelected && "active",
+					preparedRowProps.className,
+					row.original.disabled && "disabled",
+				)}
+				tabIndex={0}
+			>
+				{isDragging ? (
+					<EmptyRow colSpan={cellCount}>&nbsp;</EmptyRow>
+				) : (
+					<>
+						<td className="neo-table__dnd-td">
+							<DragHandle
+								ref={setActivatorNodeRef}
+								{...attributes}
+								{...listeners}
+							/>
+						</td>
+						{checkboxTd && (
+							<td style={{ padding: "0px 0px 0px 5px" }}>{checkboxTd}</td>
+						)}
+						{row.cells.map((cell) => {
+							const { key, ...restCellProps } = cell.getCellProps();
+							return (
+								<td {...restCellProps} key={key}>
+									{cell.render("Cell")}
+								</td>
+							);
+						})}
+					</>
+				)}
+			</tr>
+			<tr>
+				<td colSpan={cellCount}>{isDragging ? "\u00A0" : "inset table"}</td>
+			</tr>
+		</tbody>
 	);
 };
--- src/components/Table/StaticTableRow.tsx
+++ src/components/Table/StaticTableRow.tsx
@@ -13,31 +13,36 @@ export const StaticTableRow = <T extends Record<string, unknown>>({
 }) => {
 	const { key: _, ...restProps } = row.getRowProps();
 	return (
-		<tr
-			{...restProps}
-			className={clsx(
-				showDragHandle && "neo-set-keyboard-focus",
-				row.original.disabled ? "disabled" : undefined,
-			)}
-		>
-			{showDragHandle && (
-				<td className="neo-table__dnd-td">
-					<DragHandle />
-				</td>
-			)}
-			{checkboxTd && (
-				<td style={{ padding: "0px 0px 0px 5px", width: "60px" }}>
-					{checkboxTd}
-				</td>
-			)}
-			{row.cells.map((cell) => {
-				const { key, ...restCellProps } = cell.getCellProps();
-				return (
-					<td key={key} {...restCellProps}>
-						<span>{cell.render("Cell")}</span>
+		<tbody>
+			<tr
+				{...restProps}
+				className={clsx(
+					showDragHandle && "neo-set-keyboard-focus",
+					row.original.disabled ? "disabled" : undefined,
+				)}
+			>
+				{showDragHandle && (
+					<td className="neo-table__dnd-td">
+						<DragHandle />
 					</td>
-				);
-			})}
-		</tr>
+				)}
+				{checkboxTd && (
+					<td style={{ padding: "0px 0px 0px 5px", width: "60px" }}>
+						{checkboxTd}
+					</td>
+				)}
+				{row.cells.map((cell) => {
+					const { key, ...restCellProps } = cell.getCellProps();
+					return (
+						<td key={key} {...restCellProps}>
+							<span>{cell.render("Cell")}</span>
+						</td>
+					);
+				})}
+			</tr>
+			<tr>
+				<td colSpan={row.cells.length}>inset table</td>
+			</tr>{" "}
+		</tbody>
 	);
 };
