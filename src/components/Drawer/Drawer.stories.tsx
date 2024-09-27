70338d8470d61aa49a55ee8b04b63e2c8590baa6
Enrique Prado <enriquep@avaya.com>
2024-09-27 13:19:30 -0700

    More feedback from Matt

--- src/components/Drawer/Drawer.stories.tsx
+++ src/components/Drawer/Drawer.stories.tsx
@@ -285,15 +285,19 @@ export const WithNote: Story = {
 						<IconButton aria-label="send note" icon="send" key={2} />,
 					]}
 				>
-					<Note>
-						<Note.Title>Meeting notes</Note.Title>
-						<Note.Content author="Kathy" self={true}>
-							Hi, can we sync?
-						</Note.Content>
-					</Note>
-					<Note>
-						<Note.Content author="Cleo">Sure, give me 10 minutes.</Note.Content>
-					</Note>
+					<div className="drawer-container">
+						<Note>
+							<Note.Title>Meeting notes</Note.Title>
+							<Note.Content author="Kathy" self={true}>
+								Hi, can we sync?
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Content author="Cleo">
+								Sure, give me 10 minutes.
+							</Note.Content>
+						</Note>
+					</div>
 				</Drawer>
 			</main>
 		);
@@ -302,15 +306,11 @@ export const WithNote: Story = {
 
 export const CustomWidth: Story = {
 	render: () => {
-		const [defaultDrawerOpen, setDefaultDrawerOpen] = useState(false);
 		const [widthRemDrawerOpen, setWidthRemDrawerOpen] = useState(false);
 		const [widthPixelDrawerOpen, setWidthPixelDrawerOpen] = useState(false);
 
 		const toggleDrawerByName = (drawerName: string) => {
 			switch (drawerName) {
-				case "default":
-					setDefaultDrawerOpen(!defaultDrawerOpen);
-					break;
 				case "rem-units":
 					setWidthRemDrawerOpen(!widthRemDrawerOpen);
 					break;
@@ -329,46 +329,20 @@ export const CustomWidth: Story = {
 						marginBottom: "1rem",
 					}}
 				>
-					<Button onClick={() => toggleDrawerByName("default")}>
-						Open Default Width Drawer (20rem)
-					</Button>
-
 					<Button onClick={() => toggleDrawerByName("rem-units")}>
-						Open Drawer with 30rem width
+						Drawer using rem units (30rem width)
 					</Button>
 
 					<Button onClick={() => toggleDrawerByName("pixel-units")}>
-						Open Drawer with 150px width
+						Drawer using pixel units (350px width)
 					</Button>
 				</section>
 				<section>
-					<TextArea
-						helperText="Try typing here while scrim is on."
-						label="Test focus by typing here"
-						maxLength={10}
-						placeholder="Placeholder"
-						translations={{
-							over: "over",
-							remaining: "remaining",
-						}}
-					/>
+					<p>
+						These examples show how you can set custom widths for the Drawer
+						using either rem or pixel units
+					</p>
 				</section>
-
-				<Drawer
-					open={defaultDrawerOpen}
-					onClose={() => toggleDrawerByName("default")}
-					title="Drawer with default width"
-				>
-					<Note>
-						<Note.Title>Meeting notes</Note.Title>
-						<Note.Content author="Kathy" self={true}>
-							Hi, can we sync?
-						</Note.Content>
-					</Note>
-					<Note>
-						<Note.Content author="Cleo">Sure, give me 10 minutes.</Note.Content>
-					</Note>
-				</Drawer>
 
 				<Drawer
 					open={widthRemDrawerOpen}
@@ -391,7 +365,7 @@ export const CustomWidth: Story = {
 					open={widthPixelDrawerOpen}
 					onClose={() => toggleDrawerByName("pixel-units")}
 					title="Drawer width = 150px"
-					width="150px"
+					width="350px"
 				>
 					<Note>
 						<Note.Title>Meeting notes</Note.Title>
@@ -439,78 +413,96 @@ export const WithScrollbar: Story = {
 						<IconButton aria-label="send note" icon="send" key={2} />,
 					]}
 				>
-					<Note>
-						<Note.Title>Meeting notes</Note.Title>
-						<Note.Content author="Kathy" self={true}>
-							Hi, can we sync?
-						</Note.Content>
-					</Note>
-					<Note>
-						<Note.Content author="Cleo">Sure, give me 10 minutes.</Note.Content>
-					</Note>
-					<Note>
-						<Note.Title>Meeting notes</Note.Title>
-						<Note.Content author="Kathy" self={true}>
-							Hi, can we sync?
-						</Note.Content>
-					</Note>
-					<Note>
-						<Note.Content author="Cleo">Sure, give me 10 minutes.</Note.Content>
-					</Note>
-					<Note>
-						<Note.Title>Meeting notes</Note.Title>
-						<Note.Content author="Kathy" self={true}>
-							Hi, can we sync?
-						</Note.Content>
-					</Note>
-					<Note>
-						<Note.Content author="Cleo">Sure, give me 10 minutes.</Note.Content>
-					</Note>
-					<Note>
-						<Note.Title>Meeting notes</Note.Title>
-						<Note.Content author="Kathy" self={true}>
-							Hi, can we sync?
-						</Note.Content>
-					</Note>
-					<Note>
-						<Note.Content author="Cleo">Sure, give me 10 minutes.</Note.Content>
-					</Note>
-					<Note>
-						<Note.Title>Meeting notes</Note.Title>
-						<Note.Content author="Kathy" self={true}>
-							Hi, can we sync?
-						</Note.Content>
-					</Note>
-					<Note>
-						<Note.Content author="Cleo">Sure, give me 10 minutes.</Note.Content>
-					</Note>
-					<Note>
-						<Note.Title>Meeting notes</Note.Title>
-						<Note.Content author="Kathy" self={true}>
-							Hi, can we sync?
-						</Note.Content>
-					</Note>
-					<Note>
-						<Note.Content author="Cleo">Sure, give me 10 minutes.</Note.Content>
-					</Note>
-					<Note>
-						<Note.Title>Meeting notes</Note.Title>
-						<Note.Content author="Kathy" self={true}>
-							Hi, can we sync?
-						</Note.Content>
-					</Note>
-					<Note>
-						<Note.Content author="Cleo">Sure, give me 10 minutes.</Note.Content>
-					</Note>
-					<Note>
-						<Note.Title>Meeting notes</Note.Title>
-						<Note.Content author="Kathy" self={true}>
-							Hi, can we sync?
-						</Note.Content>
-					</Note>
-					<Note>
-						<Note.Content author="Cleo">Sure, give me 10 minutes.</Note.Content>
-					</Note>
+					<div className="drawer-container">
+						<Note>
+							<Note.Title>Meeting notes</Note.Title>
+							<Note.Content author="Kathy" self={true}>
+								Hi, can we sync?
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Content author="Cleo">
+								Sure, give me 10 minutes.
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Title>Meeting notes</Note.Title>
+							<Note.Content author="Kathy" self={true}>
+								Hi, can we sync?
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Content author="Cleo">
+								Sure, give me 10 minutes.
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Title>Meeting notes</Note.Title>
+							<Note.Content author="Kathy" self={true}>
+								Hi, can we sync?
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Content author="Cleo">
+								Sure, give me 10 minutes.
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Title>Meeting notes</Note.Title>
+							<Note.Content author="Kathy" self={true}>
+								Hi, can we sync?
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Content author="Cleo">
+								Sure, give me 10 minutes.
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Title>Meeting notes</Note.Title>
+							<Note.Content author="Kathy" self={true}>
+								Hi, can we sync?
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Content author="Cleo">
+								Sure, give me 10 minutes.
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Title>Meeting notes</Note.Title>
+							<Note.Content author="Kathy" self={true}>
+								Hi, can we sync?
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Content author="Cleo">
+								Sure, give me 10 minutes.
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Title>Meeting notes</Note.Title>
+							<Note.Content author="Kathy" self={true}>
+								Hi, can we sync?
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Content author="Cleo">
+								Sure, give me 10 minutes.
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Title>Meeting notes</Note.Title>
+							<Note.Content author="Kathy" self={true}>
+								Hi, can we sync?
+							</Note.Content>
+						</Note>
+						<Note>
+							<Note.Content author="Cleo">
+								Sure, give me 10 minutes.
+							</Note.Content>
+						</Note>
+					</div>
 				</Drawer>
 			</main>
 		);
