7a40962bf14ea5fe69eb91d7c8c180b49a7d6ed9
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-10-16 14:09:18 -0500

    clean up stories

--- src/components/SideNavigation/SideNavigation.stories.tsx
+++ src/components/SideNavigation/SideNavigation.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
+import clsx from "clsx";
 import { Image } from "components/Image";
 import { TopNav } from "components/TopNav";
 import { SideNavigation } from "./";
@@ -8,8 +9,7 @@ const meta: Meta<typeof SideNavigation> = {
 	component: SideNavigation,
 	title: "Components/Side Navigation",
 	args: {
-		"aria-label": "Side Navigation",
-		currentUrl: "#home",
+		currentUrl: "http://active.com",
 	},
 	argTypes: {
 		// no useful controls to add to stories
@@ -21,78 +21,99 @@ const meta: Meta<typeof SideNavigation> = {
 };
 export default meta;
 
-type Story = StoryObj<{ ariaLabel: string; currentUrl: string }>;
+export const Default: StoryObj<{
+	dir: "ltr" | "rtl";
+	mode: "neo-light" | "neo-dark";
+	showIcons: boolean;
+}> = {
+	args: {
+		dir: "ltr",
+		mode: "neo-light",
+		showIcons: true,
+	},
+	argTypes: {
+		dir: {
+			control: "radio",
+			options: ["ltr", "rtl"],
+			description: "These are props for the story, not the component.",
+			table: { category: "Story Props" },
+		},
+		mode: {
+			control: "radio",
+			options: ["neo-light", "neo-dark"],
+			description: "These are props for the story, not the component.",
+			table: { category: "Story Props" },
+		},
+		showIcons: {
+			control: "boolean",
+			description: "These are props for the story, not the component.",
+			table: { category: "Story Props" },
+		},
+	},
+	render: ({ dir, mode, showIcons }) => {
+		return (
+			<main>
+				<h2>Default Example</h2>
 
-export const Default: Story = {
-	render: ({ currentUrl }) => (
-		<main>
-			<h2>With and Without Icons</h2>
+				<p style={{ marginBottom: "1rem" }}>
+					The side navigation bar is used to display the secondary navigation
+					elements of an application. These include a list of links that can be
+					used to move between sections or pages within the application or the
+					site.
+				</p>
 
-			<p style={{ marginBottom: "1rem" }}>
-				Side Navigation is a vertical navigation component that allows users to
-				navigate between different sections of a website or application. The top
-				level items must either all have icons, or no icons.
-			</p>
+				<p style={{ marginBottom: "1rem" }}>
+					The top level items must either all have icons, or no icons.
+				</p>
 
-			<p style={{ marginBottom: "1rem" }}>
-				IMPORTANT: The links in the Side Nav are actual links. If you select one
-				it will take you to a new page.
-			</p>
+				<p style={{ marginBottom: "1rem" }}>
+					<b>IMPORTANT</b>: The links in the Side Nav are actual links. If you
+					select one it will take you to a new page.
+				</p>
 
-			<TopNav
-				logo={
-					<a href="/" title="Logo Link">
-						<Image
-							isDecorativeOrBranding
-							src="/src/components/TopNav/logo-full-light.svg"
+				<TopNav
+					logo={
+						<a href="/" title="Logo Link">
+							<Image
+								isDecorativeOrBranding
+								src="/src/components/TopNav/logo-full-light.svg"
+							/>
+						</a>
+					}
+				/>
+
+				<section dir={dir} className={clsx("neo-global-colors", mode)}>
+					<SideNavigation aria-label="side navigation">
+						<SideNavigation.TopLinkItem
+							label="Home"
+							icon={showIcons ? "home" : undefined}
+							href="#home"
 						/>
-					</a>
-				}
-			/>
-
-			<section
-				style={{
-					display: "flex",
-					justifyContent: "space-between",
-				}}
-			>
-				<SideNavigation
-					aria-label="left aligned side navigation"
-					currentUrl={currentUrl}
-				>
-					<SideNavigation.TopLinkItem label="Home" icon="home" href="#home" />
-					<SideNavigation.TopLinkItem
-						label="Dashboard"
-						icon="dashboard"
-						href="#dashboard"
-					/>
-					<SideNavigation.TopLinkItem
-						label="Advanced"
-						icon="settings"
-						href="#address-book"
-					/>
-					<SideNavigation.TopLinkItem label="Admin" icon="user" disabled />
-				</SideNavigation>
-
-				<SideNavigation
-					aria-label="right aligned side navigation"
-					currentUrl={currentUrl}
-					dir="rtl"
-				>
-					<SideNavigation.TopLinkItem label="Home" href="#home" />
-					<SideNavigation.TopLinkItem label="Dashboard" href="#dashboard" />
-					<SideNavigation.TopLinkItem label="Advanced" href="#address-book" />
-					<SideNavigation.TopLinkItem label="Admin" disabled />
-				</SideNavigation>
-			</section>
-		</main>
-	),
+						<SideNavigation.TopLinkItem
+							label="Dashboard"
+							icon={showIcons ? "dashboard" : undefined}
+							href="#dashboard"
+						/>
+						<SideNavigation.TopLinkItem
+							label="Advanced"
+							icon={showIcons ? "settings" : undefined}
+							href="#address-book"
+						/>
+						<SideNavigation.TopLinkItem
+							label="Admin"
+							icon={showIcons ? "user" : undefined}
+							disabled
+						/>
+					</SideNavigation>
+				</section>
+			</main>
+		);
+	},
 };
 
+type Story = StoryObj<{ currentUrl: string }>;
+
 export const CategoryGroups: Story = {
-	args: {
-		currentUrl: "http://active.com",
-	},
 	render: ({ currentUrl }) => (
 		<SideNavigation aria-label="Text only Nav" currentUrl={currentUrl}>
 			<SideNavigation.NavCategory expanded={true} label="Text Only Category">
