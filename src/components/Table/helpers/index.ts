9fa8774c59e25365a00fc6119ec39a22f063979c
Chunguang Yang <chungguangy@avaya.com>
2024-10-17 20:22:43 -0400

    restore row-helper export

--- src/components/Table/helpers/index.ts
+++ src/components/Table/helpers/index.ts
@@ -3,4 +3,5 @@ export * from "./default-data";
 export * from "./helpers";
 export * from "./mock-data";
 export * from "./server-mock-data";
+export * from "./row-helper";
 export * from "./useFullTableWidth";
