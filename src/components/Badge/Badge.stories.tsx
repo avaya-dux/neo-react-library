7a0ac79e332f67fe67bde56b62e8c4793d075ba1
Mohammad Razian <raziandev@Mohammads-MacBook-Air.local>
2024-10-28 20:25:13 +0100

    Picked up feedback from PR

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,14 +1,32 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
+import clsx from "clsx";
 import { Badge } from "./Badge";
 
 type PagePropsAndCustomArgs = React.ComponentProps<typeof Badge> & {
 	childType?: string;
+	icon?: string;
+	mode?: string;
 };
 
 const meta: Meta<PagePropsAndCustomArgs> = {
 	title: "Components/Badge",
 	component: Badge,
+	argTypes: {
+		data: {
+			control: "text",
+		},
+		childType: {
+			control: "select",
+			options: ["none", "icon", "tabs"],
+			description: "These are props for the story, not the component.",
+			table: { category: "Story Props" },
+		},
+	},
+	args: {
+		data: "99",
+		childType: "icon",
+	},
 	parameters: {
 		controls: {
 			exclude: ["aria-label"],
@@ -20,35 +38,79 @@ export default meta;
 
 type Story = StoryObj<PagePropsAndCustomArgs>;
 
+const TabsBadge = ({ data }: PagePropsAndCustomArgs) => (
+	<>
+		<div className="neo-tabs" role="tablist">
+			<ul className="neo-tabs__nav">
+				<Badge data={data} aria-label={`badge representing ${data}`}>
+					<li className="neo-tabs__item neo-tabs__item--active">
+						<a
+							href="/#"
+							role="tab"
+							aria-selected="true"
+							aria-controls="tab1-panel"
+							id="tab1"
+						>
+							Tab 1
+						</a>
+					</li>
+				</Badge>
+			</ul>
+		</div>
+	</>
+);
+
+const IconBadge = ({ data, icon }: PagePropsAndCustomArgs) => (
+	<Badge data={data} aria-label={`badge representing ${data}`}>
+		<span className={icon || "neo-icon-customer"} />
+	</Badge>
+);
+
+const SimpleBadge = ({ data }: PagePropsAndCustomArgs) => (
+	<Badge data={data} aria-label={`badge representing ${data}`} />
+);
+
 export const BadgeStory: Story = {
 	argTypes: {
-		data: {
+		icon: {
 			control: "text",
-			defaultValue: "99",
+			if: { arg: "childType", eq: "icon" },
 		},
-		childType: {
-			control: "select",
-			options: ["none", "icon", "tabs"],
-			defaultValue: "none",
+		mode: {
+			control: "radio",
+			options: ["neo-light", "neo-dark"],
+			description: "These are props for the story, not the component.",
+			table: { category: "Story Props" },
 		},
 	},
-	render: ({ data, childType }) => {
-		return childType === "tabs" ? (
-			<div className="neo-tabs" role="tablist">
-				<ul className="neo-tabs__nav">
-					<Badge data={data} aria-label={`badge representing ${data}`}>
-						<li className="neo-tabs__item neo-tabs__item--active">
-							<a href="/#" role="tab" aria-selected="true">
-								Tab 1
-							</a>
-						</li>
-					</Badge>
-				</ul>
-			</div>
-		) : (
-			<Badge data={data} aria-label={`badge representing ${data}`}>
-				{childType === "icon" ? <span className="neo-icon-customer" /> : ""}
-			</Badge>
-		);
+	decorators: [
+		(Story, { args }) => (
+			<>
+				<p style={{ marginBottom: "2rem" }}>
+					Badges are used to indicate the number of notifications pending for a
+					particular function, such as unread email, voicemail, text, and chat
+					messages.
+				</p>
+				<p style={{ marginBottom: "2rem" }}>
+					They can stand alone, or be used with icons or tabs.
+				</p>
+				<div
+					style={{ display: "inline-block" }}
+					className={clsx("neo-global-colors", args.mode)}
+				>
+					<Story />
+				</div>
+			</>
+		),
+	],
+	render: (props) => {
+		switch (props.childType) {
+			case "tabs":
+				return <TabsBadge {...props} />;
+			case "icon":
+				return <IconBadge {...props} />;
+			default:
+				return <SimpleBadge {...props} />;
+		}
 	},
 };
