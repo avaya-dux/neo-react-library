f8b39faebfe2e920edba4942cb81a2553a831550
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-10-29 14:43:18 -0500

    use standard handling of a11y error in Icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,6 +1,9 @@
 import clsx from "clsx";
 
-import { type IconNamesType, getIconClass } from "utils/icons";
+import { handleAccessbilityError } from "utils";
+
+import type { IconNamesType } from "utils/icons";
+import { getIconClass } from "utils/icons";
 
 // import { SizeType } from "utils/size"; TODO https://jira.forge.avaya.com/browse/NEO-645
 type SizeType = "sm" | "md" | "lg";
@@ -40,7 +43,7 @@ export const Icon: React.FC<IconProps> = ({
 	...rest
 }: IconProps) => {
 	if (!rest["aria-label"]) {
-		console.error(
+		handleAccessbilityError(
 			"A descriptive label is required for screen readers to identify the button's purpose",
 		);
 	}
@@ -67,8 +70,8 @@ export const Icon: React.FC<IconProps> = ({
 			{...rest}
 			className={clsx(
 				icon && getIconClass(icon),
-				status !== undefined && "neo-icon-state",
 				sizing,
+				status !== undefined && "neo-icon-state",
 				status !== undefined && `neo-icon-state--${status}`,
 				className,
 			)}
