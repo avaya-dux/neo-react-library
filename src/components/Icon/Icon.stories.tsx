f49a570a21640ddad1f7ff69d67d240979985aa4
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-10-31 13:05:27 -0500

    Storybook // add mode and dir to global options
    and update Icon stories

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -2,40 +2,18 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { Icon } from ".";
 import "./Icon.stories.css";
-import clsx from "clsx";
 
-type StoryMeta = React.ComponentProps<typeof Icon> & {
-	mode: "neo-light" | "neo-dark";
-};
-
-const meta: Meta<StoryMeta> = {
+type Story = StoryObj<typeof Icon>;
+const meta: Meta<typeof Icon> = {
 	component: Icon,
 	title: "Components/Icon",
 	args: {
 		icon: "info",
 		notification: false,
 		status: "available",
-
-		// story props
-		dir: "ltr",
-		mode: "neo-light",
 	},
 	argTypes: {
 		icon: { control: { type: "select" } },
-
-		// story props
-		dir: {
-			control: "radio",
-			options: ["ltr", "rtl"],
-			description: "These are props for the story, not the component.",
-			table: { category: "Story Props" },
-		},
-		mode: {
-			control: "radio",
-			options: ["neo-light", "neo-dark"],
-			description: "These are props for the story, not the component.",
-			table: { category: "Story Props" },
-		},
 
 		// hidden props
 		"aria-label": { table: { disable: true } },
@@ -43,21 +21,23 @@ const meta: Meta<StoryMeta> = {
 		notification: { table: { disable: true } },
 		size: { table: { disable: true } },
 	},
+	decorators: [
+		(Story) => (
+			<div className="icon-story-container">
+				<Story />
+			</div>
+		),
+	],
 };
 export default meta;
-
-type Story = StoryObj<StoryMeta>;
 
 export const Default: Story = {
 	argTypes: {
 		notification: { table: { disable: true } },
 		status: { table: { disable: true } },
 	},
-	render: ({ dir, mode, icon }) => (
-		<div
-			dir={dir}
-			className={clsx("icon-story-container", "neo-global-colors", mode)}
-		>
+	render: ({ icon }) => (
+		<>
 			<h2>Default Icon</h2>
 
 			<p>The Icon component has three sizes.</p>
@@ -70,7 +50,7 @@ export const Default: Story = {
 				<Icon icon={icon} size="md" aria-label="medium info" />
 				<Icon icon={icon} size="lg" aria-label="large info" />
 			</div>
-		</div>
+		</>
 	),
 };
 
@@ -78,11 +58,8 @@ export const StatusIcon: Story = {
 	args: {
 		icon: "email",
 	},
-	render: ({ dir, mode, icon, status }) => (
-		<div
-			dir={dir}
-			className={clsx("icon-story-container", "neo-global-colors", mode)}
-		>
+	render: ({ icon, status }) => (
+		<>
 			<h2>Status Icon</h2>
 
 			<p>An Icon component that has a "status" can be shown in two sizes.</p>
@@ -112,6 +89,6 @@ export const StatusIcon: Story = {
 					aria-label="large info"
 				/>
 			</div>
-		</div>
+		</>
 	),
 };
