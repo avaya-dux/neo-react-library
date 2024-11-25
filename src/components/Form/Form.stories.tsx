56fa4ce18f1b4ee6ada931991da5734093647d7e
mrazian85 <61242168+mrazian85@users.noreply.github.com>
2024-11-25 17:29:49 +0100

    Fixed bug where margin on TextInputs with addons was being cutoff on focus (#594)
    Co-authored-by: Mohammad Razian <raziandev@Mohammads-MacBook-Air.local>

--- src/components/Form/Form.stories.tsx
+++ src/components/Form/Form.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, Story } from "@storybook/react";
 
-import { TextInput } from "components";
+import { Icon, TextInput } from "components";
 
 import { Form, type FormProps } from ".";
 
@@ -29,3 +29,62 @@ const Template: Story<FormProps> = (props) => (
 
 export const Templated = Template.bind({});
 Templated.args = {};
+
+const InlineForms: Story<FormProps> = (props) => (
+	<>
+		<Form {...props} inline>
+			<TextInput
+				label="Read Only"
+				defaultValue="8881112222"
+				startAddon="+1"
+				readOnly
+			/>
+
+			<TextInput
+				label="Clearable Field"
+				defaultValue="Initial Value"
+				clearable
+				helperText="Click the clear icon inside the input."
+				startAddon={<Icon icon="call" aria-label="input icon" />}
+			/>
+		</Form>
+
+		<Form {...props} inline>
+			<TextInput
+				label="Read Only"
+				defaultValue="8881112222"
+				startAddon="+1"
+				readOnly
+			/>
+
+			<TextInput
+				label="Clearable Field"
+				defaultValue="Initial Value"
+				clearable
+				helperText="Click the clear icon inside the input."
+				endAddon={<Icon icon="call" aria-label="input icon" />}
+			/>
+		</Form>
+
+		<Form {...props} inline>
+			<TextInput
+				label="Read Only"
+				defaultValue="8881112222"
+				startAddon="+1"
+				readOnly
+			/>
+
+			<TextInput
+				label="Clearable Field"
+				defaultValue="Initial Value"
+				clearable
+				helperText="Click the clear icon inside the input."
+				startAddon="+1"
+				endAddon={<Icon icon="call" aria-label="input icon" />}
+			/>
+		</Form>
+	</>
+);
+
+export const InlineFormExample = InlineForms.bind({});
+InlineFormExample.args = {};
